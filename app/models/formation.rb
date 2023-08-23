5bd24a18a974da38fefdeb20495bbb9b7ec64f5c
thomas cambriels thomascambr@gmail.com <thomascambr@gmail.com>
2023-08-23 10:24:55 +0200

    validates faits

--- app/models/formation.rb
+++ app/models/formation.rb
@@ -1,4 +1,9 @@
 class Formation < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
+
+  validates :description, length: { minimum: 10, maximum: 1000 }
+  validates :date, presence: true
+  validates :title, presence: true
+  validates :theme, presence: true
 end
